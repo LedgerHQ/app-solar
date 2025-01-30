d7022b9d860695f7c3fcd313bb7d21fa0f170bd4
sleepdefic1t <sleepdefic1t@users.noreply.github.com>
2025-01-30 13:31:23 -0500

    tests(functional): rm 'clear pending review` fixture
    `ENABLE_PENDING_REVIEW_SCREEN` is no longer used per LedgerHQ. The functional test's fixture to clear the pending review screen must also be removed in order for tests to pass successfully.

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,10 +1,3 @@
-# from ragger.conftest import configuration
-import pytest
-from ragger.firmware import Firmware
-from ragger.navigator import NavIns, NavInsID
-
-# from ragger.backend import RaisePolicy
-
 
 ###########################
 ### CONFIGURATION START ###
@@ -20,26 +13,3 @@ from ragger.navigator import NavIns, NavInsID
 
 # Pull all features from the base ragger conftest using the overridden configuration
 pytest_plugins = ("ragger.conftest.base_conftest",)
-
-
-# Notes :
-# 1. Remove this fixture once the pending review screen is removed from the app
-# 2. This fixture clears the pending review screen before each test
-# 3. The scope should be the same as the one configured by BACKEND_SCOPE in
-# ragger/conftest/configuration.py
-@pytest.fixture(scope="class", autouse=True)
-def clear_pending_review(firmware, navigator):
-    print("Clearing pending review")
-    # Press / tap a button to clear the pending review
-    pending_review_clear = (
-        NavInsID.BOTH_CLICK  # nanosp, nanox
-        if firmware.device.startswith("nano")
-        else (
-            NavIns(NavInsID.TOUCH, (200, 545))  # stax
-            if firmware is Firmware.STAX
-            else NavIns(NavInsID.TOUCH, (240, 488))
-        )  # flex
-    )
-    navigator.navigate(
-        [pending_review_clear], screen_change_before_first_instruction=False
-    )
